1fbefb6778a6f27b1447911718efd3e702ba0a48
user04 <user04@gmail.com>
2016-03-01 15:26:31 -0400

    quinto mensaje

--- mostrar.c
+++ mostrar.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-	char mensaje_bienvenida[] = {"Bienvenido al mundo C"};
+	char mensaje_bienvenida[] = {"Hola mundo C"};
 	char mensaje_despedida[] = {"Hasta otra oportunidad"};
 	printf ("%s\n", mensaje_bienvenida);
 	imprimir_mensaje(mensaje_despedida);
